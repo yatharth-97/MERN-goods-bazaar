7bfa2201c7fbf795ed32a6eb807d36eb01cb4a11
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-14 02:37:47 +0530

    added new Component

---
+++ client/src/components/ProtectedPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+function ProtectedPage() {
+  const [user, setUser] = useState(null);
+  const validateToken = async () => {};
+
+  useEffect(() => {
+    validateToken();
+  }, []);
+
+  return <div>ProtectedPage</div>;
+}
+
+export default ProtectedPage;
