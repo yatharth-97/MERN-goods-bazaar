4fe051a5a702a4a7cfa399976ad4839ed6d5a804
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-16 20:42:09 +0530

    added Protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import ProtectedPage from './components/ProtectedPage';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <ProtectedPage>
+                <Home />
+              </ProtectedPage>
+            }
+          />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
         </Routes>
