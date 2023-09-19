b72d9d6f6134c69d694ed9b7472834a8d9c75a9e
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-20 00:21:34 +0530

    created usersSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { loaderSlice } from './loadersSlice';
+import { usersSlice } from './usersSlice';
 
 const store = configureStore({
   reducer: {
     loaders: loaderSlice.reducer,
+    users: usersSlice.reducer,
   },
 });
 
