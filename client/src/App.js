739dc7b4801a2d9aed5cb678f8465c8ba12cd2b1
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-19 23:42:10 +0530

    adding to the app

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import ProtectedPage from './components/ProtectedPage';
 import Spinner from './components/Spinner';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { loading } = useSelector((state) => state.loaders);
   return (
     <div>
       <Spinner />
