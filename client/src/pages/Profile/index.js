19746795dbfb98fc97fc024f87f67084f1718293
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-21 13:09:10 +0530

    added Tabs

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Tabs } from 'antd';
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <Tabs defaultActiveKey='1'>
+        <Tabs.TabPane tab='Buy / Sell' key='1'>
+          <h1>Buy / Sell</h1>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Bids' key='2'>
+          <h1>Bids</h1>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='General' key='3'>
+          <h1>General</h1>
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
 }
 
 export default Profile;
