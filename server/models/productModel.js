e2296f3ce665bfd8aea2b412b7e8946b000fcc36
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-27 00:32:58 +0530

    created productModel Schema

---
+++ server/models/productModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: Array,
+      default: [],
+      required: true,
+    },
+    billAvailable: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    warrantyAvailable: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    accessoriesAvailable: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    boxAvailable: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
