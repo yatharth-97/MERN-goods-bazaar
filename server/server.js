199f382f4a2992df2642bc8c2d65c82f49fffe91
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-09-28 21:21:36 +0530

    added products api

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const dbConfig = require('./config/dbConfig');
 const port = process.env.port || 5000;
 
 const usersRoute = require('./routes/usersRoute');
+const productsRoute = require('./routes/productsRoute');
+
 app.use('/api/users', usersRoute);
+app.use('/api/products', productsRoute);
 
 app.listen(port, () => {
   console.log(`Node/Express server started on port ${port}`);
